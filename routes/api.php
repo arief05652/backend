eec21f34d38506facf76bb1693f95eb9b3311ab4
arief05652 <ariefminardi140@gmail.com>
2025-05-04 07:39:49 +0700

    feat: Membuat fitur logout current user dan harus menyertakan token login

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthSystem::class, 'register']);
     Route::post('/login', [AuthSystem::class, 'login']);
+    Route::get('/logout', [AuthSystem::class, 'logout'])->middleware('auth:sanctum');
 });
 
 Route::post('/test', function(Request $request){

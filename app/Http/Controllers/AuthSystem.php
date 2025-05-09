85da3bd06adf316a574a71c284a4cff1ecd6b8ae
arief05652 <ariefminardi140@gmail.com>
2025-05-09 14:20:11 +0700

    refactor: ganti kolom dan return user id saat login

--- app/Http/Controllers/AuthSystem.php
+++ app/Http/Controllers/AuthSystem.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthSystem extends Controller
 {
-    function login(Request $request) {
+    public function login(Request $request) {
         $data = $request->validate([
             "email" => 'required|string|email',
             "password" => 'required|string'
@@ -23,7 +23,11 @@ class AuthSystem extends Controller
         }
 
         if (Hash::check($data['password'], $user->password)) {
-            return $user->createToken($request->email)->plainTextToken;
+            $token = $user->createToken($request->email)->plainTextToken;
+            return response()->json([
+                'user_id' => $user['id'],
+                'token' => $token
+            ]);
         } else {
             return response()
                 ->json([
@@ -32,12 +36,12 @@ class AuthSystem extends Controller
         }
     }
 
-    function register(Request $request) {
+    public function register(Request $request) {
         $data = $request->validate([
-            "nama_depan" => 'required|string|max:25',
-            "nama_belakang" => 'nullable|string|max:25',
+            "first_name" => 'required|string|max:25',
+            "last_name" => 'nullable|string|max:25',
             "email" => 'required|string|email',
-            "no_hp" => 'required|string|max:15',
+            "phone" => 'required|string|max:15',
             "password" => 'required|string|max:255'
         ]);
 
@@ -51,10 +55,10 @@ class AuthSystem extends Controller
 
         // Buat user baru
         User::create([
-            "nama_depan" => $data["nama_depan"],
-            "nama_belakang" => $data["nama_belakang"] ?? '',
+            "first_name" => $data["first_name"],
+            "last_name" => $data["last_name"] ?? '',
             "email" => $data["email"],
-            "no_hp" => $data["no_hp"],
+            "phone" => $data["phone"],
             "password" => Hash::make($data["password"])
         ]);
 
@@ -63,7 +67,7 @@ class AuthSystem extends Controller
         ], 201);
     }
 
-    function logout(Request $request) {
+    public function logout(Request $request) {
         $request->user()->tokens()->delete();
         return response()->json(['message' => 'Logged out successfully'], 200);
     }

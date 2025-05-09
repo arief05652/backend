b4b616ddc993a3154a072b464f4dce8c4594d5b2
arief05652 <ariefminardi140@gmail.com>
2025-05-09 14:25:37 +0700

    feat: Tambah route user untuk menampilkan dan update datanya

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,21 @@
 <?php
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\AuthSystem;
+use App\Http\Controllers\ReservationSystem;
+use App\Http\Controllers\UserSystem;
 use Illuminate\Support\Facades\Route;
 
 // AUTH PROSES
-Route::prefix('auth')->group(function () {
-    Route::post('/register', [AuthSystem::class, 'register']);
-    Route::post('/login', [AuthSystem::class, 'login']);
-    Route::get('/logout', [AuthSystem::class, 'logout'])->middleware('auth:sanctum');
+Route::prefix('auth')->group(function () { // base path
+    Route::post('/register', [AuthSystem::class, 'register']); // register user baru
+    Route::post('/login', [AuthSystem::class, 'login']); // login ke aplikasi
+    Route::delete('/logout', [AuthSystem::class, 'logout'])->middleware('auth:sanctum'); // logout system
+});
+
+// USER SELECTION
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::prefix('user')->group(function () { // base path
+        Route::get('/users/{id}', [UserSystem::class, 'user_by_id']); // select user by id
+        Route::put('/users-update/{id}', [UserSystem::class, 'update_user']); // update data user
+    });
 });

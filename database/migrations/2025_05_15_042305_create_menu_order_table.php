62e98d482a8e92a6e3d425a0eeb714ca221e2222
arief05652 <ariefminardi140@gmail.com>
2025-05-16 16:41:09 +0700

    ✨feat: struktur database untuk system menu

---
+++ database/migrations/2025_05_15_042305_create_menu_order_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('menu_name', 255)->index();
+            $table->decimal('price', 10, 2);
+            $table->text('picture');
+            $table->enum('category', ['appetizers', 'main_course', 'drinks', 'side_dishes'])->index();
+            $table->enum('status', ['out_of_stock', 'available']);
+        });
+
+        Schema::create('order', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('menu_id')->constrained('menu')->onDelete('cascade');
+            $table->foreignUuid('user_id')->constrained('user')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->string('notes', 255)->nullable();
+            $table->enum('status', ['pending', 'preparing', 'ready'])->default('pending');
+            $table->timestamp('order_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menus');
+        Schema::dropIfExists('orders');
+    }
+};

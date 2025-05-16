54d5ddc3d0bb1c4a9327915045c5d77b39d2874c
arief05652 <ariefminardi140@gmail.com>
2025-05-16 16:39:47 +0700

    ✨feat: new endpoint menu system

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthSystem;
+use App\Http\Controllers\MenuSystem;
 use App\Http\Controllers\ReservationSystem;
 use App\Http\Controllers\TableSystem;
 use App\Http\Controllers\UserSystem;
@@ -21,20 +22,17 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::patch('/update-role', [UserSystem::class, 'update_role']); // update role user
         Route::get('/show-history-reserve', [ReservationSystem::class, 'show_histori_reservation']); // show history reservation user
     });
-});
 
-// TABLE SYSTEM
-Route::middleware(['auth:sanctum'])->group(function () {
+
+    // TABLE SYSTEM
     Route::prefix('table')->group(function () {
         Route::get('/show-table', [TableSystem::class, 'show_table']); // show table
         Route::post('/add-table', [TableSystem::class, 'add_table']); // tambah table baru
         Route::patch('/update-table', [TableSystem::class, 'update_table']); // update current table
         Route::delete('/delete-table', [TableSystem::class, 'delete_table']); // delete table 
     });
-});
 
-// RESERVATION SYSTEM
-Route::middleware(['auth:sanctum'])->group(function () {
+    // RESERVATION SYSTEM
     Route::prefix('reserve')->group(function () {
         Route::post('/add-reserve', [ReservationSystem::class, 'add_reserve']); // user booking table
         Route::get('/check-user', [ReservationSystem::class, 'check_in_reserve']); // check-in user by pelayan
@@ -42,4 +40,12 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::post('/done-reserve', [ReservationSystem::class, 'done_reservation']); // change status and close menu
         Route::post('/cancel-reserve', [ReservationSystem::class, 'cancel_reservation']); // cancel reservation
     });
+
+    // MENU SYSTEM
+   Route::prefix('menu')->group(function () {
+        Route::get('/show-menu', [MenuSystem::class, 'show_menu']); // menampilkan menu
+        Route::post('/add-menu', [MenuSystem::class, 'added_menu']); // menambahkan menu
+        Route::patch('/update-menu/{id}', [MenuSystem::class, 'update_menu']); // update menu
+        Route::delete('/delete-menu', [MenuSystem::class, 'delete_menu']); // hapus menu
+    }); 
 });
